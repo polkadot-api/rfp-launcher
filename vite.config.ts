51326580012f327c5dfe53533fe849ca971dcd33
Victor Oliva <olivarra1@gmail.com>
2025-05-09 13:01:38 +0200

    publish to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_WORKFLOW ? "/rfp-launcher/" : undefined,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
